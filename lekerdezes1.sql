9d699c748e7185698acdf9cda6ef00de25c3c8ad
kurisd <tnatesco@gmail.com>
2019-11-26 10:11:59 +0100

    Update lekerdezes1.sql
    minimeptun

--- lekerdezes1.sql
+++ lekerdezes1.sql
@@ -18,3 +18,18 @@ Select count(*) AS 'Darabszám' from hallgatok;
 Select * from tantargyak;
 Select  count(tkov) AS 'darabszam', tkov AS 'tantargyi követelmeny' from tantargyak grup by tkov;
 Select count(uv) as 'uv hallgato' from jegyek where uv <>0
+
+/*minineptun*/
+Select Case kforma 
+when 1 then 'BSC'
+when 2 then 'MSC'
+when 3 then 'PHD' 
+else 'kodhiba' end as [kepzesi forma], count(*) as [hallgatoi letszam]
+from hallgatok
+group by kforma;
+
+
+select h.neptun, vnev, knev, AVG(jegy) AS 'átlag' from hallgatok h inner join jegyek j on h.neptun=j.neptun group by h.neptun ,vnev ,knev; 
+/* megjegyzes inner join left join right join */
+select h.neptun, vnev, knev, AVG(CAST (jegy as float)) AS 'átlag' from hallgatok h inner join jegyek j on h.neptun=j.neptun group by h.neptun ,vnev ,knev; 
+select h.neptun, vnev, knev, sum(cast(jegy as float)*kredit)/SUM(kredit) AS 'átlag' from hallgatok h inner join jegyek j on h.neptun=j.neptun group by h.neptun ,vnev ,knev; 
